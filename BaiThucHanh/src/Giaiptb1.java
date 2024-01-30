fd739ddb0f564d7ae43c8530078b5112286f0d69
baduc04 <daobaduc1111@gmail.com>
2024-01-30 22:04:52 +0700

    javaoop

--- BaiThucHanh/src/Giaiptb1.java
+++ BaiThucHanh/src/Giaiptb1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class giaiPTbac1 {
+public class Giaiptb1 {
     public static void main(String[] args) {
         double a, b , x ;
         Scanner sc = new Scanner(System.in);
