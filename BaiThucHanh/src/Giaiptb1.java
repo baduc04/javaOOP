4220eea7e350c1044912781c6f157e1fbe7422a7
baduc04 <daobaduc1111@gmail.com>
2024-05-11 15:19:44 +0700

    javaoop

--- BaiThucHanh/src/Giaiptb1.java
+++ BaiThucHanh/src/Giaiptb1.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 
-public class Giaiptb1 {
+public class giaiPTbac1 {
     public static void main(String[] args) {
-        double a, b , x ;
+        double a, b, x;
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhập hệ số A: ");
         a = sc.nextDouble();
         System.out.print("nhập hệ số b : ");
         b = sc.nextDouble();
-        if (a==0) {
+        if (a == 0) {
             System.out.println("không phải phương trình bậc 1 ");
-        }
-        else {
-        x= -b/a;
-        System.out.println("phương trình có nghiệm x="+x);
+        } else {
+            x = -b / a;
+            System.out.println("phương trình có nghiệm x=" + x);
 
-      } 
+        }
     }
 }
