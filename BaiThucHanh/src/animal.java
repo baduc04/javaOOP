7fbb501ac4d436726d2593e93d4107dc379e3046
baduc04 <daobaduc1111@gmail.com>
2024-05-14 23:32:34 +0700

    javaoop

---
+++ BaiThucHanh/src/animal.java
@@ -0,0 +1,15 @@
+
+abstract class Animal {
+    abstract void run();
+}
+
+class dog extends Animal {
+    void run() {
+        System.out.println("hab ");
+    }
+
+    public static void main(String args[]) {
+        Animal abc = new dog();
+        abc.run();
+    }
+}
